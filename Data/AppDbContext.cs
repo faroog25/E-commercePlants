43e4abe00861dfb3b35a78c4dab775f0b5a3d756
faroog25 <faroogbajahaw25@gmail.com>
2025-01-15 15:02:22 +0300

    seed pages

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,5 +6,18 @@ namespace E_commercePlants.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options):DbContext(options)
     {
         public DbSet<Page> Pages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Page>().HasData(
+
+                new Page { Id = 1, Title = "Home", Slug = "home", Body = "This is the home page" },
+                new Page { Id = 2, Title = "About", Slug = "about", Body = "This is the about page" },
+                new Page { Id = 3, Title = "Services", Slug = "services", Body = "This is the services page" },
+                new Page { Id = 4, Title = "Contact", Slug = "contact", Body = "This is the contact page" }
+
+                );
+        }
     }
 }
--- Migrations/20250115120148_SeedPages.Designer.cs
+++ Migrations/20250115120148_SeedPages.Designer.cs
@@ -3,6 +3,7 @@ using E_commercePlants.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_commercePlants.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250115120148_SeedPages")]
+    partial class SeedPages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
