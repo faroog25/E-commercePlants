78236430b45aad102347785ed577c5b8e8b6731a
faroog25 <faroogbajahaw25@gmail.com>
2025-01-15 14:52:55 +0300

    add database context and add page model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commercePlants.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options):DbContext(options)
+    {
+        public DbSet<Page> Pages { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using E_commercePlants.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbCon"));
+});
 
 var app = builder.Build();
 
