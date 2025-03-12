8af7c4d1d90030e70de2e1fdffdf777c8f03a6b0
faroog25 <faroogbajahaw25@gmail.com>
2025-03-12 22:59:46 +0300

    feat: Add product creation form and update layout
    Implemented a new product creation form in Create.cshtml, including fields for name, description, price, category, and image upload. Updated _Layout.cshtml to include a link to the products section. Modified Product.cs to adjust validation attributes and added a new CSS rule for image preview styling. Added a binary image file for visual assets.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using E_commercePlants.Data.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace E_commercePlants.Models
@@ -21,7 +22,7 @@ namespace E_commercePlants.Models
 
         public string Image { get; set; } = "noimage.png";
         [NotMapped]
-        [FileExtensions]
+        [FileExtension]
         public IFormFile ImageUpload { get; set; }
 
     }
