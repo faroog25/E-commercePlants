83737a0cf7ff18f74b4e0abc44c0a54b1994f9b8
faroog25 <faroogbajahaw25@gmail.com>
2025-03-14 23:58:05 +0300

    feat: Introduce SmallCartViewModel for cart functionality
    - Added a new SmallCartViewModel class to represent the cart's item count and total amount, facilitating better data management for the shopping cart feature.

---
+++ Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,8 @@
+namespace E_commercePlants.Models.ViewModels;
+
+public class SmallCartViewModel
+{
+    public int NumberOfItems { get; set; }
+    public decimal TotalAmount { get; set; }
+    
+}
