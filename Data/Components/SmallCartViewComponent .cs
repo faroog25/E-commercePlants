00c9e1fdd549960f091961e90ce846370bdd0c02
faroog25 <faroogbajahaw25@gmail.com>
2025-03-15 00:03:04 +0300

    feat: Implement SmallCartViewComponent for displaying cart summary
    - Added SmallCartViewComponent to render a summary of the shopping cart, including the number of items and total amount.
    - Utilized session management to retrieve cart items and calculate the summary, enhancing the user experience with real-time cart updates.

---
+++ Data/Components/SmallCartViewComponent .cs
@@ -0,0 +1,32 @@
+﻿using CMSECommerce.Infrastructure;
+using E_commercePlants.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commercePlants.Data.Components
+{
+    public class SmallCartViewComponent():ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            SmallCartViewModel smallCartViewModel;
+
+            if (cart is null || cart.Count==0)
+            {
+                smallCartViewModel = null;
+            }
+            else
+            {
+                smallCartViewModel = new SmallCartViewModel
+                {
+                    NumberOfItems = cart.Sum(x => x.Quantity),
+                    TotalAmount=cart.Sum(x=>x.Quantity*x.Price)
+                };
+            }
+
+            return View(smallCartViewModel);
+        }
+    }
+}
