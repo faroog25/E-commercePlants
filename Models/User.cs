5688550f9546b46b48a9355c2f37a495910dd410
faroog25 <faroogbajahaw25@gmail.com>
2025-03-15 17:18:07 +0300

    feat: Add User model for user registration and management
    - Created a new User class in the Models directory to define user properties for registration, including validation attributes for username, email, password, confirm password, phone, and address.
    - Implemented data annotations to enforce validation rules, enhancing user input handling and ensuring data integrity.

---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commercePlants.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Username is required and must be at least 2 characters long"),MinLength(2)]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required and must be at least 5 characters long"),MinLength(5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+    }
+}
