af16210f48c38743ef71e8d768c66052ee4fc334
faroog25 <faroogbajahaw25@gmail.com>
2025-03-16 23:45:48 +0300

    feat: Implement AccountController for user registration
    - Added AccountController with a Register action that returns the registration view.
    - This sets the foundation for user registration functionality in the application.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_commercePlants.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
