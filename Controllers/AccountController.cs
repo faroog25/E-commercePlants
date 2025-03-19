3318f3686df58d9fd165d8ddf089b13df11f3446
faroog25 <faroogbajahaw25@gmail.com>
2025-03-19 21:25:47 +0300

    feat: Enhance AccountController with login functionality
    - Updated AccountController to include SignInManager for user authentication.
    - Added Login action methods for GET and POST requests, utilizing LoginViewModel for handling login data.
    - Implemented sign-in logic with error handling for invalid credentials, improving user experience during login.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using E_commercePlants.Models;
+using E_commercePlants.Models.ViewModels;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace E_commercePlants.Controllers
 {
-    public class AccountController(UserManager<ApplicationUser> userManager) : Controller
+    public class AccountController(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager) : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager = signInManager;
 
         public IActionResult Register()
         {
@@ -35,9 +40,29 @@ namespace E_commercePlants.Controllers
                 {
                     ModelState.AddModelError("",error.Description);
                 }
-
             }
             return View(user);
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel {ReturnUrl =returnUrl});
+        }
+         [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                SignInResult signInResult =await _signInManager
+                    .PasswordSignInAsync(loginViewModel.UserName,loginViewModel.Password,false,false);
+                if (signInResult.Succeeded)
+                {
+                    return Redirect(loginViewModel.ReturnUrl??"/");
+                }
+                ModelState.AddModelError("","Invalid username or password");
+            }
+             
+            return View(loginViewModel);
         }
     }
 }
