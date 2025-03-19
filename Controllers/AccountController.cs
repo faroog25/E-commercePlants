75321b036d8b23f8fa03823a8122a114938fe006
faroog25 <faroogbajahaw25@gmail.com>
2025-03-19 16:49:43 +0300

    feat: Enhance AccountController for user registration
    - Refactored AccountController to include UserManager for managing user accounts.
    - Implemented asynchronous registration logic, allowing users to create accounts with validation and role assignment.
    - Added error handling to display validation messages during the registration process, improving user experience.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_commercePlants.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController(UserManager<IdentityUser> userManager) : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser newUser= new IdentityUser {
+                    UserName=user.UserName,Email=user.Email,
+                    PhoneNumber=user.Phone
+                    
+                };
+                IdentityResult result = await _userManager.CreateAsync(newUser
+                ,user.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(newUser ,"Customer");
+                    Redirect("/");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+
+            }
+            return View(user);
         }
     }
 }
