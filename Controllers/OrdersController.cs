a0ed9744a476a55c102675d0a8a7f6ff970127a4
faroog25 <faroogbajahaw25@gmail.com>
2025-03-20 01:13:11 +0300

    feat: Implement OrdersController for order management
    - Created OrdersController to handle order creation and management, integrating with the session cart to process user orders.
    - Added methods for displaying the order index and creating new orders, enhancing the order processing functionality in the e-commerce application.

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,52 @@
+﻿using E_commercePlants.Data;
+using E_commercePlants.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace E_commercePlants.Controllers
+{
+    public class OrdersController(AppDbContext context,SignInManager<ApplicationUser> signInManager) : Controller
+    {
+        private readonly AppDbContext _context = context;
+        private readonly SignInManager<ApplicationUser> signInManager = signInManager;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Create()
+        {
+             List<CartItem> cart = HttpContext.Session
+                .GetJson<List<CartItem>>("Cart") ;
+            Order order = new Order{
+                Username=User.Identity.Name,GrandTotal=cart.Sum(x=>x.Price*x.Quantity)
+            };
+            _context.Add(order);
+            await _context.SaveChangesAsync();
+
+            foreach (var item in cart)
+            {
+                OrderDetail orderDetail=new()
+                {
+                    OrderId=order.Id,
+                    ProductId=item.ProductId,
+                    ProductName=item.ProductName,
+                    Quantity=item.Quantity,
+                    Price=item.Price,
+                    Image=item.Image
+                };
+
+                _context.Add(orderDetail);
+            await _context.SaveChangesAsync();
+
+            }
+            HttpContext.Session.Remove("Cart");
+
+            return Redirect("/orders/index");
+        }
+
+    }
+}
