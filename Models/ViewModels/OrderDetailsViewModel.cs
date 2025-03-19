5ba2234e8b96eef5a7bbbef0fdd1940ac130d7bf
faroog25 <faroogbajahaw25@gmail.com>
2025-03-20 02:01:48 +0300

    feat: Create OrderDetailsViewModel for order detail representation
    - Added OrderDetailsViewModel class to encapsulate order and order details, facilitating data transfer for order-related views in the e-commerce application.

---
+++ Models/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,8 @@
+namespace E_commercePlants.Models.ViewModels;
+
+public class OrderDetailViewModel
+{
+    public Order Order { get; set; }
+    public List<OrderDetail> OrderDetails { get; set; }
+    
+}
