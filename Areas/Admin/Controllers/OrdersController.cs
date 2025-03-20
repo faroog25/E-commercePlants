66055a8f36db02a3ff413cb8ee9114822ed780a3
faroog25 <faroogbajahaw25@gmail.com>
2025-03-20 03:09:54 +0300

    feat: Implement Orders management functionality in Admin area
    - Created OrdersController to manage order listings and update shipping status, enhancing order management capabilities for administrators.
    - Added Index view to display a list of orders with relevant details and actions, improving the user interface for order oversight.
    - Updated layout to include navigation link for Orders, facilitating easier access for admin users.

---
+++ Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using E_commercePlants.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace E_commercePlants.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize("Admin")]
+
+    public class OrdersController(AppDbContext context) : Controller
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+
+            List<Order> orders = await _context.Orders.OrderByDescending(x=>x.Id).ToListAsync();
+
+
+            return View(orders);
+        }
+       
+       [HttpPost]
+        public async Task<IActionResult> ShippedStatus(int id,bool shipped)
+        {
+            Order order=await _context.Orders.FirstOrDefaultAsync(x=>x.Id==id);
+
+            order.Shipped=shipped;
+            _context.Update(order);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
